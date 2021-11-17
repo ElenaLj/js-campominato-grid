77bf2803d5fec3cc0f6a3f3250a202048a83ddd5
ElenaLj <elenaljubijankic@gmail.com>
2021-11-17 23:22:56 +0100

    insert griglia switch

--- js/script.js
+++ js/script.js
@@ -1,1 +1,41 @@
 // alert("Hello World!")
+
+
+// variabile del pulsante play
+const myButton = document.getElementById("play");
+
+let items = "";
+
+myButton.addEventListener("click", function (){
+
+    //variabile della selezione del livello
+    const selectLevel = document.getElementById("select").value;
+    console.log(selectLevel);
+
+    // variabile del container
+    const myContainer = document.querySelector(".container");
+
+    //switch per la selezione del livello
+
+    switch (selectLevel) {
+        case "easy":
+        for (let i = 1; i <= 100; i++) {
+            myContainer.innerHTML += `
+            <div class="cube easy">${i}</div>`
+        }
+        break;
+
+        case "medium":
+        for (let i = 1; i <= 81; i++) {
+            myContainer.innerHTML += `
+            <div class="cube medium">${i}</div>`
+        }
+        break;
+
+        case "hard":
+        for (let i = 1; i <= 49; i++) {
+            myContainer.innerHTML += `
+            <div class="cube hard">${i}</div>`
+        }
+    }
+});
